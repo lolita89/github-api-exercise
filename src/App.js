227dfc5c81a7b18c5dfadd77bb4e770b2cde7248
lolita89 <cika.lolita@hotmail.com>
2018-08-08 22:38:10 +0200

    component spotify api seperated

--- src/App.js
+++ src/App.js
@@ -1,77 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import ApiGithub from './components/ApiGithub';
-import SpotifyWebApi from "spotify-web-api-js";
+import ApiSpotify from './components/ApiSpotify';
 
-const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-  constructor(){
-    super();
-    const params = this.getHashParams();
-    const token = params.access_token;
-  
-    if (token) {
-    spotifyApi.setAccessToken(token);
-  }
-  
-  this.state = {
-    loggedIn: token ? true : false,
-    nowPlaying: { name: 'Not Checked', albumArt: '' }
-  }
-}
 
-  getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
-    }
-    return hashParams;
-  }
-
-  getNowPlaying(){
-    spotifyApi.getMyCurrentPlaybackState()
-      .then((response) => {
-        this.setState({
-          nowPlaying: { 
-              name: response.item.name, 
-              albumArt: response.item.album.images[0].url
-            }
-        });
-      })
-  }
-  
   render() {
     return (
     <div className="row">
-      <div className="column">
-      <ApiGithub>  
-      </ApiGithub>
-      </div>
-
-      <div className="column">
-        <h1>It will be another component here....</h1>
-        <a href='http://localhost:8888'> Login to Spotify </a>
-
-        <div>
-          Now Playing: { this.state.nowPlaying.name }
+        
+        <div className="column">
+          <ApiGithub>  
+          </ApiGithub>
         </div>
 
-        <div>
-          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
+        <div className="column">
+          <ApiSpotify> 
+          </ApiSpotify>
         </div>
-
-        { this.state.loggedIn &&
-        <button onClick={() => this.getNowPlaying()}>
-          Check Now Playing
-        </button>
-      }
-
-      </div>
+        
     </div>
     );
   }
---
+++ src/components/ApiSpotify.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import './ApiSpotify.css';
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
+
+class ApiSpotify extends Component {
+    constructor(){
+      super();
+      const params = this.getHashParams();
+      const token = params.access_token;
+    
+      if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' }
+    }
+  }
+  
+    getHashParams() {
+      var hashParams = {};
+      var e, r = /([^&;=]+)=?([^&;]*)/g,
+          q = window.location.hash.substring(1);
+      e = r.exec(q)
+      while (e) {
+         hashParams[e[1]] = decodeURIComponent(e[2]);
+         e = r.exec(q);
+      }
+      return hashParams;
+    }
+  
+    getNowPlaying(){
+      spotifyApi.getMyCurrentPlaybackState()
+        .then((response) => {
+          this.setState({
+            nowPlaying: { 
+                name: response.item.name, 
+                albumArt: response.item.album.images[0].url
+              }
+          });
+        })
+    }
+    
+    render() {
+      return (
+        <div> 
+            <h1>It is an exercise to call spotify API</h1>
+            <a href='http://localhost:8888'> Login to Spotify </a>
+
+            <div>
+                <p> Now Playing: { this.state.nowPlaying.name } </p>
+            </div>
+  
+            <div>
+                <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
+            </div>
+  
+            { this.state.loggedIn &&
+            <button className="button-spotify" onClick={() => this.getNowPlaying()}>
+                Check Now Playing!
+            </button>
+            }
+
+        </div>
+      );
+    }
+  }
+
+export default ApiSpotify;
