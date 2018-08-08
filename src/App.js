3456acbd5adaba4436e3d77f34a4c3504547b41a
lolita89 <cika.lolita@hotmail.com>
2018-08-08 21:36:08 +0200

    successful calling the spotify API for checkNowPlaying

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 import ApiGithub from './components/ApiGithub';
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+  
+    if (token) {
+    spotifyApi.setAccessToken(token);
+  }
+  
+  this.state = {
+    loggedIn: token ? true : false,
+    nowPlaying: { name: 'Not Checked', albumArt: '' }
+  }
+}
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying(){
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: { 
+              name: response.item.name, 
+              albumArt: response.item.album.images[0].url
+            }
+        });
+      })
+  }
   
   render() {
     return (
@@ -12,8 +53,24 @@ class App extends Component {
       </ApiGithub>
       </div>
 
-      <div>
+      <div className="column">
         <h1>It will be another component here....</h1>
+        <a href='http://localhost:8888'> Login to Spotify </a>
+
+        <div>
+          Now Playing: { this.state.nowPlaying.name }
+        </div>
+
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
+        </div>
+
+        { this.state.loggedIn &&
+        <button onClick={() => this.getNowPlaying()}>
+          Check Now Playing
+        </button>
+      }
+
       </div>
     </div>
     );
